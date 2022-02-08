9e45ca6215289418e95d1a11a75ffa7cc8d16710
Ian Robertson <isrobertson21@gmail.com>
2022-02-08 00:44:17 -0500

    created main.cpp file with text

---
+++ visualstudiocode/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world\n";
+    std::cout << "This is just testing visual studio code and manually adding files";
+    
+    return 0;
+}
